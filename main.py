623ae6db4a8c6522f507d641929a19f7b0e3e203
Andrei Muzikant <andrei.muzikant@gmail.com>
2025-04-30 20:26:37 +0300

    Added print name

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime
 def main():
     print("Welcome to Git Assignment")
     print("Current date and time:", datetime.datetime.now())
+    print("Andrei")
 
 if __name__ == "__main__":
     main()
