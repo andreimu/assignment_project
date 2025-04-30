753a0e05d7e1ce9c605564d89680bfd714d04c16
andreimu <andrei.muzikant@gmail.com>
2025-04-30 20:27:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime
 def main():
     print("Welcome to Git Assignment")
     print("Current date and time:", datetime.datetime.now())
+    print("Muzikant")
 
 if __name__ == "__main__":
     main()
